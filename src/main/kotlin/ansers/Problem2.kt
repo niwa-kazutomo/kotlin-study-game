643d15f9d8068648a6718504a24e482b8f144ce9
Kazutomo Niwa <niwa_kazutomo@gs2.io>
2017-09-08 22:16:54 +0900

    二問目の回答例を追加

---
+++ src/main/kotlin/ansers/Problem2.kt
@@ -0,0 +1,31 @@
+package ansers
+
+import runtime.IGame
+import java.awt.Color
+import java.awt.Graphics
+
+class Problem2 : IGame() {
+
+    var x: Int = 0
+    var y: Int = 0
+
+    override fun process(pushedKey: Int) {
+        if(isPushKey(KEY_UP)) {
+            y -= 1
+        }
+        if(isPushKey(KEY_DOWN)) {
+            y += 1
+        }
+        if(isPushKey(KEY_LEFT)) {
+            x -= 1
+        }
+        if(isPushKey(KEY_RIGHT)) {
+            x += 1
+        }
+    }
+
+    override fun render(g: Graphics) {
+        g.color = Color.RED
+        g.fillRect(x, y, 50, 50)
+    }
+}
